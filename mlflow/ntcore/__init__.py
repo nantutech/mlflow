f4e66857ad8571f694b5860a2043fa1baf194f1f
dsp-columbus <-add>
2021-03-26 10:41:07 -0400

    Handle missing workspace id

--- mlflow/ntcore/__init__.py
+++ mlflow/ntcore/__init__.py
@@ -17,14 +17,18 @@ def set_workspace_id(workspace_id):
     Set global workspace id.
     """
     os.environ['NTCORE_WORKSPACE_ID'] = workspace_id
-    _logger.info('NTCORE_WORKSPACE_ID={0}'.format(os.environ['NTCORE_WORKSPACE_ID']))
     
 
 def _emit_model_to_ntcore(estimator, parameters, metrics):
     """
     Sends metrics to ntcore server.
     """
-    workspace_id = os.environ['NTCORE_WORKSPACE_ID']
+    try:
+        workspace_id = os.environ['NTCORE_WORKSPACE_ID']
+    except Exception as envError:
+        _logger.warning('This experiment is not logged in ntcore server because no workspace id is found. You can use mflow.set_workspace_id(...) to set the workspace id.')
+        return
+
     endpoint = BASE_URL.format(ntcore_host=NTCORE_HOST, workspace_id=workspace_id)
     model_blob = pickle.dumps(estimator)
     payload = { "parameters": json.dumps(parameters),
@@ -33,4 +37,4 @@ def _emit_model_to_ntcore(estimator, parameters, metrics):
     try:
         requests.post(endpoint, data=payload)
     except ConnectionError as connectionError:
-        _logger.warning('ntcore server is not up.')
+        _logger.warning('This experiment is not logged in ntcore server because ntcore server is not running.')
